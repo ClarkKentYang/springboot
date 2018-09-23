c386b707217525686a6a50d32b7b8a7eed47676d
ClarkKentYang <lykaka22@qq.com>
2018-09-23 14:02:45 +0800

    new springboot on git

---
+++ src/main/java/com/milan/HelloWorldMainApplication.java
@@ -0,0 +1,16 @@
+package com.milan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+    @SpringBootApplication 标注主程序类
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+
+}
