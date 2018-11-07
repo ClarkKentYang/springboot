25e07999a9e59c2ce8ce872b1dbc477d75891dbb
ClarkKentYang <lykaka22@qq.com>
2018-11-07 23:11:56 +0800

    springboot about config

--- src/main/java/com/milan/HelloWorldMainApplication.java
+++ src/main/java/com/milan/HelloWorldMainApplication.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.ImportResource;
 public class HelloWorldMainApplication {
 
     public static void main(String[] args) {
+        //分支
         SpringApplication.run(HelloWorldMainApplication.class,args);
     }
 
